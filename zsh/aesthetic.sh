6ea72e3965f7bce18a4fd3aef572f725fd13ed5f
Dante Tobar <dante.m.tobar@gmail.com>
2022-01-11 18:55:41 -0800

    Add kitty configuration

--- zsh/aesthetic.sh
+++ zsh/aesthetic.sh
@@ -5,3 +5,9 @@ export CLICOLOR=1
 export LSCOLORS=fxxxxxxxxxxxxxxxxxxxxx
 
 alias ls="ls -G" # better ls colors
+
+# changes cursor back to a beam on vim exit
+_fix_cursor() {
+   echo -ne '\e[5 q'
+}
+precmd_functions=(_fix_cursor)
