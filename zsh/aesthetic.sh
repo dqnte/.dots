1d830b5d7eb23a63274fcb41565972ea5780f3c4
Dante Tobar <dante.m.tobar@gmail.com>
2022-01-21 14:42:23 -0800

    Modify zsh colors

--- zsh/aesthetic.sh
+++ zsh/aesthetic.sh
@@ -4,18 +4,18 @@ parse_git_branch() {
    branch=$(git branch 2> /dev/null)
    branch=$(sed -e '/^[^*]/d' <<< $branch)
    branch=$(sed -e 's/* \(.*\)/\1/' <<< $branch)
-   branch=$(sed -r 's/^[a-z]*-//' <<< $branch)
-   branch=$(sed 's/^\([a-z]*-[0-9]*\).*/\1/' <<< $branch)
+   branch=$(sed -r 's/^[a-z]*-//' <<< $branch) # remove initials
+   branch=$(sed 's/^\([a-z]*-[0-9]*\).*/\1/' <<< $branch) # show ticket
    if [ -z $branch ]
    then
       echo ""
    else
-      echo "($branch) "
+      echo "%F{blue}($branch)%f "
    fi
 }
 
 export PS1="\$(parse_git_branch)%1~ $ " # leading text in shell
-export LSCOLORS=fxxxxxxxxxxxxxxxxxxxxx
+export LSCOLORS=cxxxxxxxxxxxxxxxxxxxxx
 alias ls="ls -G" # better ls colors
 
 # changes cursor back to a beam on vim exit
