94dd5f96d38d1de98e5200b3d556484fcfda522d
Dante Tobar <dante.m.tobar@gmail.com>
2022-01-22 09:20:19 -0800

    Move pyenv symbol to arrowa

--- zsh/aesthetic.sh
+++ zsh/aesthetic.sh
@@ -4,19 +4,21 @@ parse_git() {
    branch=$(git branch --show-current 2> /dev/null | cut -d '-' -f 2-3)
    if [ $branch ]
    then
-      echo "%F{blue}($branch)%f "
+      echo "($branch) "
    fi
 }
 
 parse_venv() {
    if [[ $PATH == *"pyenv-virtualenv"* ]]
    then
-      echo "%F{blue}%f "
+      echo "+>"
+   else
+      echo ">"
    fi
 }
 
 export PYENV_VIRTUALENV_DISABLE_PROMPT=1
-export PS1="\$(parse_venv)\$(parse_git)%1~ $ " # leading text in shell
+export PS1="%F{blue}\$(parse_git)%f%1~ \$(parse_venv) "
 export LSCOLORS=cxxxxxxxxxxxxxxxxxxxxx
 alias ls="ls -G" # better ls colors
 
