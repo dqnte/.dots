e468237c637bd03745efb85bebcead1ea1086ab9
Dante Tobar <dante.m.tobar@gmail.com>
2022-01-24 10:21:33 -0800

    Variable instantiation fix

--- zsh/init.sh
+++ zsh/init.sh
@@ -2,7 +2,7 @@
 if command -v tmux &> /dev/null && [ -n "$PS1" ] && [[ ! "$TERM" =~ screen ]] && [[ ! "$TERM" =~  tmux ]] && [ -z "$TMUX" ]; then
     exec tmux -f ~/.dots/tmux.conf
 else
-    DOTZSH = "$HOME/.dots/zsh"
+    DOTZSH="$HOME/.dots/zsh"
     source "$DOTZSH/environment.sh"
     source "$DOTZSH/aesthetic.sh"
     source "$DOTZSH/utils.sh"
