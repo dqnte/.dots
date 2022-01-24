5db4d9ae9f9ccb35a4a8f5617e760d986b2cd46c
Dante Tobar <dante.m.tobar@gmail.com>
2022-01-24 09:53:04 -0800

    Add git completion to zsh

--- zsh/init.sh
+++ zsh/init.sh
@@ -2,10 +2,12 @@
 if command -v tmux &> /dev/null && [ -n "$PS1" ] && [[ ! "$TERM" =~ screen ]] && [[ ! "$TERM" =~  tmux ]] && [ -z "$TMUX" ]; then
     exec tmux -f ~/.dots/tmux.conf
 else
-    source "$HOME/.dots/zsh/environment.sh"
-    source "$HOME/.dots/zsh/aesthetic.sh"
-    source "$HOME/.dots/zsh/utils.sh"
-    source "$HOME/.dots/zsh/aliases.sh"
+    DOTZSH = "$HOME/.dots/zsh"
+    source "$DOTZSH/environment.sh"
+    source "$DOTZSH/aesthetic.sh"
+    source "$DOTZSH/utils.sh"
+    source "$DOTZSH/aliases.sh"
+    source "$DOTZSH/completion.sh"
 
     # Pluggins
     zsh_add_plugin "lukechilds/zsh-nvm"
