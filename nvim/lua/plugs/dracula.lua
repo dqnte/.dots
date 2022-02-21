fcab71a03293170d7518766e1df5298c35a16c79
Dante Tobar <dante.m.tobar@gmail.com>
2022-02-20 22:10:59 -0700

    Add number line highlighting to dracula theme

--- nvim/lua/plugs/dracula.lua
+++ nvim/lua/plugs/dracula.lua
@@ -3,6 +3,9 @@ Plug('dracula/vim', { name = 'dracula' })
 if (vim.env.THEME == 'dracula') then
     vim.o.background = 'dark'
     vim.g.dracula_colorterm = 0
-    nvim_cmd("autocmd ColorScheme dracula highlight Cursorline ctermbg=233")
+
+    local drac_highlight = "autocmd ColorScheme dracula highlight "
+    nvim_cmd(drac_highlight .. "Cursorline ctermbg=233")
+    nvim_cmd(drac_highlight .. "CursorlineNR ctermbg=233 cterm=none")
     nvim_cmd("autocmd User PlugLoaded ++nested colorscheme dracula")
 end
