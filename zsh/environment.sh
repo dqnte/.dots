2a9a084077fca926577890e7fa151e840890143a
Dante Tobar <dante.m.tobar@gmail.com>
2022-02-22 15:25:25 -0700

    Add kubectl completion to zsh

--- zsh/environment.sh
+++ zsh/environment.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 PATH="$PATH:~/bin"
 
-# Add Visual Studio Code (code)
+# Visual Studio Code (code)
 export PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
 
-export PATH="$HOME/.poetry/bin:$PATH" # poetry cli
-export PATH="/usr/local/opt/postgresql@11/bin:$PATH" # psql cli
+# PSQL cli
+export PATH="/usr/local/opt/postgresql@11/bin:$PATH"
 
 # Python Setup
 export PYENV_ROOT=/usr/local/var/pyenv
-
-# Go Setup
-export GOPATH=$HOME/go-workspace # don't forget to change your path correctly!
-export GOROOT=/usr/local/opt/go/libexec
-export PATH=$PATH:$GOPATH/bin
-export PATH=$PATH:$GOROOT/bin
+export PATH="$HOME/.poetry/bin:$PATH"
 
 # Lazy load pyenv - gotta work on it tho
 function pyenv() {
@@ -27,3 +22,9 @@ function pyenv() {
 function pyclean() {
     find . -regex "\(.*__pycache__.*\|*.py[co]\)" -delete
 }
+
+# Go Setup
+export GOPATH=$HOME/go-workspace # don't forget to change your path correctly!
+export GOROOT=/usr/local/opt/go/libexec
+export PATH=$PATH:$GOPATH/bin
+export PATH=$PATH:$GOROOT/bin
