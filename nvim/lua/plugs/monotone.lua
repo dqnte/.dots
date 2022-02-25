b23af6f8b6cf834a0c78c00ba25b5d173449e1c1
Dante Tobar <dante.m.tobar@gmail.com>
2022-02-25 06:26:43 -0700

    Make monotone greyscale

--- nvim/lua/plugs/monotone.lua
+++ nvim/lua/plugs/monotone.lua
@@ -1,12 +1,63 @@
 Plug('Lokaltog/vim-monotone')
 
+local colors = {
+  black        = '#121111',
+  white        = '#d2cfcf',
+  red          = '#7788aa',
+  green        = '#88aa77',
+  blue         = '#d2cfcf',
+  yellow       = '#d2cfcf',
+  gray         = '#d2cfcf',
+  darkgray     = '#504945',
+  lightgray    = '#767676',
+  inactivegray = '#262626',
+}
+
+local a = { bg = colors.gray, fg = colors.black, gui = 'bold'}
+local b = { bg = colors.darkgray, fg = colors.white }
+local c = { bg = colors.inactivegray, fg = colors.gray }
+
+local theme = {
+  normal = {
+    a = a,
+    b = b,
+    c = c
+  },
+  insert = {
+    a = a,
+    b = b,
+    c = c
+  },
+  visual = {
+    a = a,
+    b = b,
+    c = c
+  },
+  replace = {
+    a = a,
+    b = b,
+    c = c
+  },
+  command = {
+    a = a,
+    b = b,
+    c = c
+  },
+  inactive = {
+    a = {bg = colors.darkgray, fg = colors.gray, gui = 'bold'},
+    b = {bg = colors.darkgray, fg = colors.gray},
+    c = {bg = colors.darkgray, fg = colors.gray}
+  }
+}
+
 if (vim.env.THEME == 'mono') then
     vim.o.background = 'dark'
-    vim.o.termguicolors = false
-    vim.luatheme = "auto"
+    vim.luatheme = theme
     vim.loaded.start_mono = function()
         vim.g.monotone_emphasize_comments = 1
         nvim_cmd("colorscheme monotone")
-        nvim_cmd("hi CursorLineNR ctermbg=234")
+        nvim_cmd("hi Comment guifg=#767676")
+        nvim_cmd("hi NonText guifg=#5c5c5c")
+        nvim_cmd("hi EndOfBuffer guifg=#2b2b2b")
     end
 end
