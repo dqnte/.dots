9c5bc73811427bb2780b238e2766337fce883059
Dante Tobar <dante.m.tobar@gmail.com>
2022-02-25 20:51:21 -0700

    Round off palenight

--- nvim/lua/plugs/palenight.lua
+++ nvim/lua/plugs/palenight.lua
@@ -5,5 +5,7 @@ if (vim.env.THEME == "palenight") then
     vim.luatheme = "auto"
     vim.loaded.start_palenight = function()
         nvim_cmd("colorscheme palenight")
+        nvim_cmd("hi Normal guibg=#181a24")
+        nvim_cmd("hi Cursorline guibg=#1f2430")
     end
 end
