355054af80929e6a2c4be87e7ce078c54fc33d6d
Dante Tobar <dante.m.tobar@gmail.com>
2022-03-13 15:15:21 -0700

    Lighten dracula theme background

--- nvim/lua/plugs/dracula.lua
+++ nvim/lua/plugs/dracula.lua
@@ -2,11 +2,10 @@ Plug('dracula/vim', { name = 'dracula' })
 
 if (vim.env.THEME == 'dracula') then
     vim.o.background = 'dark'
-    vim.g.dracula_colorterm = 0
-
     vim.loaded.start_dracula = function()
         nvim_cmd("colorscheme dracula")
-        nvim_cmd("hi Cursorline ctermbg=233 guibg=#121212")
-        nvim_cmd("hi CursorlineNR ctermbg=233 guibg=#121212")
+        nvim_cmd("hi Cursorline ctermbg=233 guibg=#22242e")
+        nvim_cmd("hi CursorlineNR ctermbg=233 guibg=#22242e")
+        nvim_cmd("hi Normal guibg=#15161c")
     end
 end
