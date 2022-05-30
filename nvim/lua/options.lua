4af9c31d3d76de8551d17ae391a26fc39f64d63d
Dante Tobar <dante.m.tobar@gmail.com>
2022-05-30 13:34:31 -0700

    Make number line toggleable

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,29 +1,32 @@
 -- Basic Settings
 local options = {
 	showmatch = true,
-	ignorecase = true,              -- case insensitive
+	ignorecase = true, -- case insensitive
 	smartcase = true,
-	incsearch = true,               -- incremental search
+	incsearch = true, -- incremental search
 	hlsearch = false,
-	tabstop = 4,                    -- number of columns occupied by a tab
+	tabstop = 4, -- number of columns occupied by a tab
 	softtabstop = 4,
-	expandtab = true,               -- converts tabs to white space
-	shiftwidth = 4,                 -- width for autoindents
-	autoindent = true,              -- indent a new line the same amount as the line just typed
-	number = true,                  -- add line numbers
-	wildmode = "longest,list",      -- get bash-like tab completions
-	syntax="on",                    -- syntax highlighting
-	mouse="a",                      -- enable mouse click
-	clipboard="unnamedplus",        -- using system clipboard
-	ttyfast = true,                 -- Speed up scrolling in Vim
-	swapfile = false,               -- disable creating swap file
-	signcolumn = "yes",             -- prevent sign column from shifting
-	encoding="UTF-8",
-	updatetime=300,
-    cursorline = true,
-    termguicolors = true,
+	expandtab = true, -- converts tabs to white space
+	shiftwidth = 4, -- width for autoindents
+	autoindent = true, -- indent a new line the same amount as the line just typed
+	number = false, -- line numbers
+	wildmode = "longest,list", -- get bash-like tab completions
+	syntax = "on", -- syntax highlighting
+	mouse = "a", -- enable mouse click
+	clipboard = "unnamedplus", -- using system clipboard
+	ttyfast = true, -- Speed up scrolling in Vim
+	swapfile = false, -- disable creating swap file
+	signcolumn = "yes", -- prevent sign column from shifting
+	encoding = "UTF-8",
+	updatetime = 300,
+	cursorline = true,
+	termguicolors = true,
 }
 
+-- toggle number line
+keymap("n", "<leader>nn", "<cmd>set number!<CR>", { noremap = true, silent = true })
+
 for k, v in pairs(options) do
-  vim.opt[k] = v
+	vim.opt[k] = v
 end
