2bc84f20a962b870e27b52ccb834921fe8be053f
Dante Tobar <dante.m.tobar@gmail.com>
2022-06-13 10:39:27 -0700

    Add keymap to toggle lualine

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -22,10 +22,32 @@ local options = {
 	updatetime = 300,
 	cursorline = true,
 	termguicolors = true,
+    scrolloff = 8,
+    sidescrolloff = 8,
 }
 
 -- toggle number line
 keymap("n", "<leader>nn", "<cmd>set number!<CR>", { noremap = true, silent = true })
+
+-- minimal mode
+vim.g.hidden_all = 0
+vim.g.hide_all = function()
+    if vim.g.hidden_all == 0 then
+        vim.g.hidden_all = 1
+        vim.opt.showmode = false
+        vim.opt.ruler = false
+        vim.opt.laststatus = 0
+        vim.opt.showcmd = false
+    else
+        vim.g.hidden_all = 0
+        vim.opt.showmode = true
+        vim.opt.ruler = true
+        vim.opt.laststatus = 2
+        vim.opt.showcmd = true
+    end
+end
+
+keymap("n", "<leader>q", "<cmd>lua vim.g.hide_all()<CR>", { noremap = true, silent = true })
 
 for k, v in pairs(options) do
 	vim.opt[k] = v
