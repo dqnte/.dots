c406cc52ee495c607b55914c869469035e52f1f5
Dante Tobar <dante.m.tobar@gmail.com>
2022-06-13 10:35:18 -0700

    Set hop shortcuts to single keypress

--- nvim/lua/plugs/hop.lua
+++ nvim/lua/plugs/hop.lua
@@ -4,17 +4,15 @@ vim.after.start_hop = function()
 	require("hop").setup()
 
     -- keyboard shortcuts
-	keymap("n", "<leader>ee", "<cmd> HopWord<cr>", {})
-	keymap("v", "<leader>ee", "<cmd> HopWord<cr>", {})
-	keymap("n", "<leader>ew", "<cmd> HopLine<cr>", {})
-	keymap("v", "<leader>ew", "<cmd> HopLine<cr>", {})
+	keymap("n", "<leader>e", "<cmd> HopWord<cr>", {})
+	keymap("v", "<leader>e", "<cmd> HopWord<cr>", {})
 
     -- keymap for word end jump
     local lua_require = "<cmd> lua require'hop'.hint_words"
 	local props = "({ hint_position = require'hop.hint'.HintPosition.END })"
-	keymap("n", "<leader>es", lua_require .. props .. "<cr>", {})
+	keymap("n", "<leader>w", lua_require .. props .. "<cr>", {})
 	props = "({ hint_position = require'hop.hint'.HintPosition.END })"
-	keymap("v", "<leader>es", lua_require .. props .. "<cr>", {})
+	keymap("v", "<leader>w", lua_require .. props .. "<cr>", {})
 
     -- color overrides
 	nvim_cmd("hi default link HopUnmatched Comment")
