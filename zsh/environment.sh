ff33cb4322da580f0c9fe3a7b20be3273818af24
Dante Tobar <dante.m.tobar@gmail.com>
2022-06-21 07:33:29 +0200

    Add Rust to terminal env

--- zsh/environment.sh
+++ zsh/environment.sh
@@ -29,3 +29,6 @@ export GOPATH=$HOME/go-workspace # don't forget to change your path correctly!
 export GOROOT=/usr/local/opt/go/libexec
 export PATH=$PATH:$GOPATH/bin
 export PATH=$PATH:$GOROOT/bin
+
+# Rust Setup
+export PATH=$PATH:$HOME/.cargo/bin
