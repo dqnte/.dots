8de466a9d12cb06da96b7e27bf98f3313ea2ac5f
Dante Tobar <dante.m.tobar@gmail.com>
2022-10-08 16:55:16 -0700

    Update neovim to 0.8

--- nvim/lua/plugs/lspconfig.lua
+++ nvim/lua/plugs/lspconfig.lua
@@ -2,7 +2,7 @@ Plug("neovim/nvim-lspconfig")
 Plug("williamboman/nvim-lsp-installer")
 
 local function lsp_highlight_document(client)
-	if client.resolved_capabilities.document_highlight then
+	if client.server_capabilities.document_highlight then
 		vim.api.nvim_exec(
 			[[
       augroup lsp_document_highlight
@@ -32,7 +32,7 @@ local function on_attach(client, bufnr)
 	lsp_highlight_document(client)
 
 	-- disables lsp formatting so only null-ls formats
-	client.resolved_capabilities.document_formatting = false
+	client.server_capabilities.documentFormattingProvider = false
 end
 
 local function configure_server(server_name, opts)
