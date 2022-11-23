0caa41e4b1c4a612f1d4a022de7576b7048062ca
Dante Tobar <dante.m.tobar@gmail.com>
2022-11-23 11:35:28 -0800

    Use fast prettier and eslint binaries

--- nvim/lua/plugs/null_ls.lua
+++ nvim/lua/plugs/null_ls.lua
@@ -1,12 +1,5 @@
 Plug("jose-elias-alvarez/null-ls.nvim")
 
--- To install stylua:
---
--- Install rust:
--- curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
--- Install stylua:
--- cargo install stylua
-
 vim.loaded.start_null_ls = function()
 	local null_ls = require("null-ls")
 	null_ls.setup({
@@ -16,9 +9,8 @@ vim.loaded.start_null_ls = function()
 			-- null_ls.builtins.diagnostics.mypy,
 			null_ls.builtins.formatting.black,
 			-- js/ts
-			null_ls.builtins.diagnostics.eslint,
-			null_ls.builtins.formatting.prettier,
-            null_ls.builtins.formatting.eslint,
+			null_ls.builtins.formatting.prettierd,
+            null_ls.builtins.formatting.eslint_d,
 			-- lua
 			null_ls.builtins.formatting.stylua,
 		},
