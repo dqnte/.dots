e5120f0960fa45d9db482bdbe3824885c5da9150
Dante Tobar <dante.m.tobar@gmail.com>
2023-01-15 11:21:15 -0800

    Remove Telescope background in rose-pine colorscheme

--- nvim/lua/colors/rose-pine.lua
+++ nvim/lua/colors/rose-pine.lua
@@ -4,5 +4,8 @@ if vim.env.THEME == "rose-pine" then
     vim.loaded.start_rosepine = function()
         vim.o.background = vim.env.THEME_MODE
         vim.cmd('colorscheme rose-pine')
+		nvim_cmd("hi TelescopeBorder guifg=#dfdad9 guibg=none")
+		nvim_cmd("hi TelescopeNormal guifg=#797593 guibg=none")
+		nvim_cmd("hi TelescopePromptNormal guifg=#575279 guibg=none")
     end
 end
