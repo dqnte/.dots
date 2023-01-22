74afbce8594d2db1d40977528dd456f1d177bb85
Dante Tobar <dante.m.tobar@gmail.com>
2023-01-21 17:08:50 -0800

    Add custom telescope window

--- nvim/lua/plugs/telescope.lua
+++ nvim/lua/plugs/telescope.lua
@@ -7,12 +7,76 @@ keymap("n", "<leader>fb", "<cmd>lua vim.g.telescope_wrapper('buffers')<cr>", { n
 keymap("n", "<leader>fg", "<cmd>lua vim.g.telescope_wrapper('live_grep')<cr>", { noremap = true, silent = true })
 keymap("n", "<leader>gs", "<cmd>lua vim.g.telescope_wrapper('git_status')<cr>", { noremap = true, silent = true })
 
-vim.g.telescope_wrapper = function(method)
-	local theme = ""
-	if vim.fn.winwidth("%") <= 90 then
-		theme = "theme=dropdown"
+vim.g.get_flat = function(opts)
+	opts = opts or {}
+
+	require("nvim-web-devicons")
+
+	local theme_opts = {
+		theme = "flat",
+
+		results_title = false,
+
+		sorting_strategy = "ascending",
+		layout_strategy = "horizontal",
+		prompt_prefix = " ",
+		selection_caret = "  ",
+		layout_config = {
+			preview_cutoff = 1,
+			height = 60,
+			prompt_position = "top",
+		},
+
+		border = true,
+		borderchars = {
+			prompt = { " ", " ", " ", " ", " ", " ", " ", " " },
+			results = { " ", " ", " ", " ", " ", " ", " ", " " },
+			preview = { " ", " ", " ", " ", " ", " ", " ", " " },
+		},
+	}
+
+	return vim.tbl_deep_extend("force", theme_opts, opts)
+end
+
+local set_custom_highlights = function()
+	local get_color = require("utils").get_color
+	local shift_color = require("utils").shift_color
+
+	local prompt_bg = get_color("Folded", "bg#")
+	local prompt_fg = get_color("Normal", "fg#")
+	local bg_color = get_color("Normal", "bg#")
+	local prompt_title = get_color("DiagnosticWarn", "fg#")
+
+	local result_bg = shift_color(bg_color:gsub("#", ""), 3)
+    if vim.o.background == "dark" then
+		result_bg = shift_color(bg_color:gsub("#", ""), -3)
 	end
-	nvim_cmd("Telescope " .. method .. " " .. theme .. " hidden=true")
+
+	hi("TelescopePromptBorder guifg=" .. prompt_bg .. " guibg=" .. prompt_bg)
+	hi("TelescopePromptNormal guibg=" .. prompt_bg .. " guifg=" .. prompt_fg)
+	hi("TelescopePromptCounter guibg=" .. prompt_bg)
+
+	hi("TelescopePromptTitle guifg=" .. prompt_bg .. " guibg=" .. prompt_title)
+	hi("TelescopePromptPrefix guifg=" .. prompt_title .. " guibg=" .. prompt_bg)
+
+	hi("TelescopeResultsBorder guifg=" .. result_bg .. " guibg=" .. result_bg)
+	hi("TelescopePreviewBorder guifg=" .. result_bg .. " guibg=" .. result_bg)
+	hi("TelescopePreviewTitle guifg=" .. result_bg .. " guibg=" .. result_bg)
+	hi("TelescopeResultsNormal guibg=" .. result_bg)
+	hi("TelescopeResultsNumber guibg=" .. result_bg)
+	hi("TelescopeNormal guibg=" .. result_bg)
+end
+
+vim.g.telescope_wrapper = function(method)
+	local layout_strategy = "horizontal"
+	if vim.fn.winwidth("%") <= 90 then
+		layout_strategy = "vertical"
+	end
+
+	set_custom_highlights()
+
+	local caller = require("telescope.builtin")[method]
+	caller(vim.g.get_flat({ layout_strategy = layout_strategy }))
 end
 
 vim.after.start_telescope = function()
--- nvim/lua/utils.lua
+++ nvim/lua/utils.lua
@@ -4,3 +4,29 @@ autocmd = vim.api.nvim_create_autocmd
 buf_keymap = vim.api.nvim_buf_set_keymap
 user_command = vim.api.nvim_create_user_command
 Plug = vim.fn["plug#"]
+
+hi = function(group)
+	nvim_cmd("hi " .. group)
+end
+
+local utils = {}
+
+local fn = vim.fn
+utils.get_color = function(group, attr)
+	return fn.synIDattr(fn.synIDtrans(fn.hlID(group)), attr)
+end
+
+local function clamp(component)
+	return math.min(math.max(component, 0), 255)
+end
+
+utils.shift_color = function(col, amt)
+	local num = tonumber(col, 16)
+	local r = math.floor(num / 0x10000) + amt
+	local g = (math.floor(num / 0x100) % 0x100) + amt
+	local b = (num % 0x100) + amt
+    local str  = string.format("%#x", clamp(r) * 0x10000 + clamp(g) * 0x100 + clamp(b))
+    return "#" .. str:gsub("0x","")
+end
+
+return utils
