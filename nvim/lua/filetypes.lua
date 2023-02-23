4300488f0f5ccd0f1cff68bd7f49f20c36a8c23f
Dante Tobar <dante.m.tobar@gmail.com>
2023-02-22 16:01:26 -0800

    Remove statusline disable from filetypes

--- nvim/lua/filetypes.lua
+++ nvim/lua/filetypes.lua
@@ -7,18 +7,8 @@ vim.cmd([[autocmd FileType vue setlocal shiftwidth=2 softtabstop=2 expandtab]])
 
 -- txt
 autocmd({ "BufRead", "BufNewFile" }, { pattern = "*.txt", command = "setlocal signcolumn=yes:2" }) -- better margins
-autocmd(
-	{ "BufRead", "BufNewFile" },
-	{ pattern = "*.txt", command = "setlocal laststatus=0 noshowcmd noruler noshowmode" }
-) -- remove status line
 
 -- Markdown
 Plug("godlygeek/tabular")
 Plug("preservim/vim-markdown")
-autocmd(
-	{ "BufRead", "BufNewFile" },
-	{ pattern = "*.md", command = "setlocal laststatus=0 noshowcmd noruler noshowmode" }
-) -- remove status line
--- vim.cmd([[autocmd FileType markdown setlocal spell]])
--- vim.cmd([[autocmd BufRead,BufNewFile *.md setlocal spell]])
 vim.g.vim_markdown_folding_disabled = 1
