4d3052a230685275d4748f6a6fece7b5b3c71183
Dante Tobar <dante.m.tobar@gmail.com>
2024-02-20 13:50:40 -0500

    Allow gb alias to delete branches

--- scripts/gb.sh
+++ scripts/gb.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-# this can't be an alias because of how we create the fzf header
-git branch | grep '^ ' | tr -d ' ' | fzf --header " $(git branch --show-current)" | xargs git checkout
+if [ "$1" = "-d" ]; then
+    git branch | grep '^ ' | tr -d ' ' | fzf --header " $(git branch --show-current)" -m | xargs git branch -d
+elif [ "$1" = "-D" ]; then
+    git branch | grep '^ ' | tr -d ' ' | fzf --header " $(git branch --show-current)" -m | xargs git branch -D
+else
+    git branch | grep '^ ' | tr -d ' ' | fzf --header " $(git branch --show-current)" -m | xargs git checkout
+fi
