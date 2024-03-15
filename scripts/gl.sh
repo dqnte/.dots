80afddbceebf1453550bc47ebf4e5a1736c45a2c
Dante Tobar <dante.m.tobar@gmail.com>
2024-03-15 10:14:51 -0400

    Remove highlighting for parantheticals in gl alias

--- scripts/gl.sh
+++ scripts/gl.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+# this was picking up incorrect parantheses... need to fix
+# -e "s/^\([[:graph:]]*\) (\(.*\))/\1 ($ZITALICS$ZDIM\2$ZPLAIN)/" \
 git log --oneline -n 10 --decorate | \
     sed \
-    -e "s/^\([[:graph:]]*\) (\(.*\))/\1 ($ZITALICS$ZDIM\2$ZPLAIN)/" \
     -e "s/^[[:graph:]]* /$ZPADDING$ZYELLOW&$ZPLAIN /"
