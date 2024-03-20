bc02aa40474ad9d67cfaa2558d3f6e16ac4dc615
Dante Tobar <dante.m.tobar@gmail.com>
2024-03-20 17:41:26 -0400

    Check for .venv binaries of python linters

--- nvim/lua/langserver.lua
+++ nvim/lua/langserver.lua
@@ -1,6 +1,16 @@
 require("plugs/lspconfig")
 require("plugs/cmp")
 
+local function find_poetry_bin_path(cmd)
+	local file = vim.fn.findfile(".venv/bin/" .. cmd, ";Fabric;Code")
+
+	if file == "" then
+		return cmd
+	else
+		return file
+	end
+end
+
 lazy({
 	"jose-elias-alvarez/null-ls.nvim",
 	config = function()
@@ -10,8 +20,12 @@ lazy({
 			sources = {
 				-- python
 				-- null_ls.builtins.diagnostics.flake8,
-				null_ls.builtins.formatting.black,
-				null_ls.builtins.diagnostics.mypy,
+				null_ls.builtins.formatting.black.with({
+					command = find_poetry_bin_path("black"),
+				}),
+				null_ls.builtins.diagnostics.mypy.with({
+					command = find_poetry_bin_path("mypy"),
+				}),
 				-- js/ts
 				null_ls.builtins.formatting.prettierd,
 				null_ls.builtins.formatting.eslint_d,
@@ -19,6 +33,8 @@ lazy({
 				null_ls.builtins.formatting.stylua,
 				-- rust
 				null_ls.builtins.formatting.rustfmt,
+				-- kotlin
+				null_ls.builtins.formatting.ktlint,
 			},
 		})
 	end,
