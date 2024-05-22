9b879cc0e46e3dcf3fcce21f65119bc37e79ce79
Dante Tobar <dante.m.tobar@gmail.com>
2024-05-21 23:46:06 -0400

    Update preview style in z and ga

--- scripts/z_ls.sh
+++ scripts/z_ls.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
 
-MARGIN=10%
+MARGIN=5%
 
-WINDOW_POSITION="down"
+WINDOW_POSITION="top"
 [  $(tput cols) -gt 150  ] && WINDOW_POSITION="right"
 
 
 HEADER=" $ZBOLD$(pwd | sed "s=$HOME=$ZYELLOW~$ZWHITE=")"
-PREVIEW="if [ -d '{}' ]; then; ls {}; else; cat {}; fi"
-lines=$(echo " "; LS -ap )
+PREVIEW="if [ -d '{}' ]; then; ls -F {}; else; cat {}; fi"
+lines=$(LS -ap)
 choice="$(echo $lines | \
     grep -v -x -F './' | \
     fzf --header=$HEADER \
         --margin $MARGIN \
-        --header-lines=1 \
         --preview=$PREVIEW \
         --preview-window=$WINDOW_POSITION \
     )"
 while [ -d "$choice" ];
 do
     cd $choice
-    lines=$(echo " "; LS -ap )
+    lines=$(LS -ap)
     HEADER=" $ZBOLD$(pwd | sed "s=$HOME=$ZYELLOW~$ZWHITE=")"
-    choice="$(echo $lines | grep -v -x -F './' | fzf --header=$HEADER --margin $MARGIN --header-lines=1 --preview=$PREVIEW --preview-window=$WINDOW_POSITION)"
+    choice="$(echo $lines | grep -v -x -F './' | fzf --header=$HEADER --margin $MARGIN --preview=$PREVIEW --preview-window=$WINDOW_POSITION)"
 done
 
 if [ ! -z "$choice" ]; then
