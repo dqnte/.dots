67623468c1537e93ae523213a585fd0c5e8ad563
Dante Tobar <dante.m.tobar@gmail.com>
2024-05-30 12:53:08 -0400

    Add preview window to gr

--- scripts/gr.sh
+++ scripts/gr.sh
@@ -1,8 +1,11 @@
-choices=$(source "$HOME/.dots/scripts/gs.sh" \
+WINDOW_POSITION="top"
+
+[  $(tput cols) -gt 150  ] && WINDOW_POSITION="right"
+
+
+source "$HOME/.dots/scripts/gs.sh" \
     | fzf --ansi -m --preview='echo {} | sed "s/...\(.*\)$/\1/" | xargs git diff --color | tail -n +3' \
     --preview-window="$WINDOW_POSITION" \
     --header-lines=2 \
-    | awk 'NF>1{print $NF}'  \
-)
+    | awk 'NF>1{print $NF}'
 
-echo "$choices"
