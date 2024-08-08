de5b21e3c879bc085a377ca5fdce79801b69a25e
Dante Tobar <dante.m.tobar@gmail.com>
2024-08-08 14:47:48 -0400

    Add ga . to commands

--- scripts/ga.sh
+++ scripts/ga.sh
@@ -2,6 +2,12 @@ WINDOW_POSITION="top"
 
 [  $(tput cols) -gt 150  ] && WINDOW_POSITION="right"
 
+if [ "$1" == "." ]; then
+    git add .
+    source "$HOME/.dots/scripts/gs.sh" -p
+    exit
+fi
+
 choices=$(source "$HOME/.dots/scripts/gs.sh" \
     | fzf --ansi -m --preview='echo {} | sed "s/...\(.*\)$/\1/" | xargs git diff --color | tail -n +3' \
     --preview-window="$WINDOW_POSITION" \
