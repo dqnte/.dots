2c907ce13efb9d543ba8cf4abb07a6b92177b968
Dante Tobar <dante.m.tobar@gmail.com>
2025-01-28 00:10:15 -0500

    Truncate long file names with elipsis

--- scripts/fancy_ls.sh
+++ scripts/fancy_ls.sh
@@ -1,5 +1,8 @@
+len=$(tput cols)
+
 ls -ho --color=always -D '%y-%m-%d %T ' $@ \
     | tail -n +2 \
     | sort -r \
     | cut -w -f 4- \
-    | sed -e "s/^\([[:graph:]]*\)/$ZPADDING$ZYELLOW\1$ZPLAIN/"
+    | sed -e "s/^\([[:graph:]]*\)/$ZPADDING$ZYELLOW\1$ZPLAIN/" \
+    | awk -v len=$len '{ if (length($0) > len) print substr($0, 1, len) "..."; else print; }'
